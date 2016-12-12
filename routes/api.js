eec71f5c5aea1ce4b54f4d1423de9dae91ad526a
mateorotim <lichedy@gmail.com>
2016-12-12 20:51:16 +0100

    added authenticate function

--- routes/api.js
+++ routes/api.js
@@ -4,29 +4,34 @@ var router = express.Router();
 var rpio = require('rpio');
 rpio.open(3, rpio.OUTPUT, rpio.HIGH);
 
-var user = {"username":"admin","password":"admin"};
+var user = { "username": "admin", "password": "admin" };
 
-router.get('/test', function(req, res, next){
+router.get('/test', function (req, res, next) {
     res.send('API');
 });
 
 router.post('/control', function (req, res) {
-  if (!req.body) return res.sendStatus(400);
-  var username = req.headers.username;
-  var password = req.headers.password;
-  if(username == user.username && password == user.password){
-    toggleRelay(res, req.body.relay, req.body.state);
-  } else res.sendStatus(400);
+    if (!req.headers) return res.sendStatus(400);
+    if (!req.body) return res.sendStatus(400);
+    if (authenticate(req.headers.username, req.headers.password) == true) {
+        toggleRelay(res, req.body.relay, req.body.state);
+    } else res.sendStatus(400);
 });
 
-var toggleRelay = function(res, relay, state){ 
-  if(state == 1){ 
-    rpio.write(relay, rpio.HIGH);
-    return res.sendStatus(200);
-  } else if(state==0){ 
-    rpio.write(relay, rpio.LOW);
-    return res.sendStatus(200);
-  } else return res.sendStatus(400);
+var authenticate = function (username, password) {
+    if (username == user.username && password == user.password) {
+        return true;
+    } else return false;
+}
+
+var toggleRelay = function (res, relay, state) {
+    if (state == 1) {
+        rpio.write(relay, rpio.HIGH);
+        return res.sendStatus(200);
+    } else if (state == 0) {
+        rpio.write(relay, rpio.LOW);
+        return res.sendStatus(200);
+    } else return res.sendStatus(400);
 }
 
 module.exports = router;
