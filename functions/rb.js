d5579885d9787254ebb46d96e91b1ca3a1106f25
mateorotim <lichedy@gmail.com>
2016-12-17 19:02:56 +0100

    minor change

--- functions/rb.js
+++ functions/rb.js
@@ -2,120 +2,109 @@ var JsonDB = require('node-json-db');
 var rpio = require('rpio');
 var db = new JsonDB('../database/db', true, true);
 
-var rb = function () {
-    var auth = function (req) {
-        var data = db.getData("/");
-        for (let user of data.users) {
-            if (user.username == req.headers.username && user.password == req.headers.password) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    var rpioWrite = function (req, res) {
-        var relayIndex = findRelay(req.body.name);
-        if (relayIndex === null) {
-            console.log("Can't find relay");
-            return res.send(400);
-        } else {
-            var relay = {
-                "name": req.body.name,
-                "pin": req.body.pin,
-                "state": 1
-            }
-            if (relay.state === 1) {
-                rpio.write(relay.pin, rpio.LOW);
-                console.log("pin %s set to HIGH", relay.pin);
-                db.push('/relays[' + relayIndex + ']', relay);
-                return res.send({
-                    "toggled": true,
-                    "name": relay.name,
-                    "pin": relay.pin,
-                    "state": "HIGH"
-                });
-            } else if (relay.state === 0) {
-                rpio.write(relay.pin, rpio.HIGH);
-                console.log("pin %s set to LOW", relay.pin);
-                db.push('/relays[' + relayIndex + ']', relay);
-                return res.send({
-                    "toggled": true,
-                    "name": relay.name,
-                    "pin": relay.pin,
-                    "state": "LOW"
-                });
-            } else {
-                console.log("Bad pin state!");
-                return res.send(400);
-            }
+exports.auth = function (req) {
+    var data = db.getData("/");
+    for (let user of data.users) {
+        if (user.username == req.headers.username && user.password == req.headers.password) {
+            return true;
         }
     }
+    return false;
+}
 
-    var readRelays = function (res) {
-        var data = db.getData("/");
-        if (data.relays && data.relays.length > 0) {
-            res.send(data.relays);
-        }
-    }
-
-    var addRelay = function (req, res) {
+exports.rpioWrite = function (req, res) {
+    var relayIndex = findRelay(req.body.name);
+    if (relayIndex === null) {
+        console.log("Can't find relay");
+        return res.send(400);
+    } else {
         var relay = {
             "name": req.body.name,
             "pin": req.body.pin,
             "state": 1
         }
-        if (findRelay(relay.name) === null) {
-            res.send({
-                "added": false,
-                "reason": "already exists"
+        if (relay.state === 1) {
+            rpio.write(relay.pin, rpio.LOW);
+            console.log("pin %s set to HIGH", relay.pin);
+            db.push('/relays[' + relayIndex + ']', relay);
+            return res.send({
+                "toggled": true,
+                "name": relay.name,
+                "pin": relay.pin,
+                "state": "HIGH"
+            });
+        } else if (relay.state === 0) {
+            rpio.write(relay.pin, rpio.HIGH);
+            console.log("pin %s set to LOW", relay.pin);
+            db.push('/relays[' + relayIndex + ']', relay);
+            return res.send({
+                "toggled": true,
+                "name": relay.name,
+                "pin": relay.pin,
+                "state": "LOW"
             });
         } else {
-            db.push('/relays[]', relay);
-            rpioOpen(relay.pin, relay.state);
-            res.send({ "added": true });
+            console.log("Bad pin state!");
+            return res.send(400);
         }
     }
+}
 
-    var findRelay = function (name) {
-        var data = db.getData("/");
-        var i = 0;
+exports.readRelays = function (res) {
+    var data = db.getData("/");
+    if (data.relays && data.relays.length > 0) {
+        res.send(data.relays);
+    }
+}
+
+exports.addRelay = function (req, res) {
+    var relay = {
+        "name": req.body.name,
+        "pin": req.body.pin,
+        "state": 1
+    }
+    if (findRelay(relay.name) === null) {
+        res.send({
+            "added": false,
+            "reason": "already exists"
+        });
+    } else {
+        db.push('/relays[]', relay);
+        rpioOpen(relay.pin, relay.state);
+        res.send({ "added": true });
+    }
+}
+
+exports.findRelay = function (name) {
+    var data = db.getData("/");
+    var i = 0;
+    for (let relay of data.relays) {
+        if (relay.name == name) {
+            return i;
+        }
+        i++;
+    }
+    return null;
+}
+
+exports.rpioOpen = function (pin, state) {
+    if (state === 1) {
+        rpio.open(pin, rpio.OUTPUT, rpio.LOW);
+        console.log("pin %s set to OUTPUT and HIGH", pin);
+    } else if (state === 0) {
+        rpio.open(pin, rpio.OUTPUT, rpio.HIGH);
+        console.log("pin %s set to OUTPUT and LOW", pin);
+    } else console.log("Bad pin state!");
+}
+
+exports.pinInit = function () {
+    var data = db.getData("/");
+    console.log(data.relays);
+    if (data.relays && data.relays.length > 0) {
         for (let relay of data.relays) {
-            if (relay.name == name) {
-                return i;
-            }
-            i++;
-        }
-        return null;
-    }
-    var pinInit = function () {
-        var data = db.getData("/");
-        console.log(data.relays);
-        if (data.relays && data.relays.length > 0) {
-            for (let relay of data.relays) {
-                rpioOpen(relay.pin, relay.state);
-            }
+            rpioOpen(relay.pin, relay.state);
         }
     }
+}
 
-    var rpioOpen = function (pin, state) {
-        if (state === 1) {
-            rpio.open(pin, rpio.OUTPUT, rpio.LOW);
-            console.log("pin %s set to OUTPUT and HIGH", pin);
-        } else if (state === 0) {
-            rpio.open(pin, rpio.OUTPUT, rpio.HIGH);
-            console.log("pin %s set to OUTPUT and LOW", pin);
-        } else console.log("Bad pin state!");
-    }
-
-    return {
-        auth: auth,
-        rpioWrite: rpioWrite,
-        readRelays: readRelays,
-        addRelay: addRelay,
-        findRelay: findRelay,
-        pinInit: pinInit,
-        rpioOpen: rpioOpen
-    };
-};
-
-module.exports = rb;
+var exports = module.exports = {};
